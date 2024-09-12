6115aee3b325516b7122281820e049dc0b07d8bc
Juhana Pikkarainen <juhana.pikkarainen@gmail.com>
2024-09-12 11:18:42 -0400

    match case for action, add works

--- tasktracker.py
+++ tasktracker.py
@@ -1,15 +1,19 @@
 import argparse
+import datetime
 import json
+import logging
 import os
 import pathlib
+from typing import Tuple
 
 
 RESOURCE_PATH = pathlib.Path.cwd() / 'res'
 TASKS_JSON = RESOURCE_PATH / 'tasks.json'
+LOGGER = logging.getLogger("Task Tracker")
             
-def load_tasks():
+def load_tasks_from_json():
     with open(TASKS_JSON) as f:
-            tasks = json.load(f)
+        tasks = json.load(f)
     return tasks
 
 def create_task_json():
@@ -29,16 +33,22 @@ def get_args() -> argparse.ArgumentParser:
     args = parser.parse_args()
     return args
 
-def load_task_json():
+def load_tasks() -> list[str]:
     try:
-        tasks = load_tasks()
+        tasks = load_tasks_from_json()
     except FileNotFoundError:
         create_task_json()
-        tasks = load_tasks()
-    return tasks
+        tasks = load_tasks_from_json()
+    return tasks["tasks"]
 
-def choose_action(args: argparse.Namespace):
-    action, value = None, None
+def save_tasks_to_json(tasks: list):
+    task_list = {'tasks': tasks}
+    with open(TASKS_JSON, mode='w') as f:
+        json.dump(task_list, f, indent=2)
+
+def choose_action(args: argparse.Namespace) -> Tuple[str, str]:
+    action: str = ''
+    value: str = ''
     if args.add:
         action = 'add'
         value = args.add
@@ -51,16 +61,64 @@ def choose_action(args: argparse.Namespace):
     elif args.list:
         action = 'list'
         value = args.list
+    else:
+        # This should already be safeguarded by the mutex arg group, but safe is safe.
+        action = 'unknown'
     return (action, value)
 
+def _add(value: str):
+    tasks = load_tasks()
+    new_id = get_new_task_id(tasks)
+    creation_time = datetime.datetime.now(tz=datetime.timezone.utc)
+    new_task = {
+        "id": new_id,
+        "description": value,
+        "createdAt": creation_time.isoformat(),
+        "updatedAt": creation_time.isoformat()
+    }
+    tasks.append(new_task)
+    save_tasks_to_json(tasks)
+    LOGGER.info(f'Task added successfully (ID: {new_id})')
+
+def get_new_task_id(tasks):
+    task_ids = [task["id"] for task in tasks]
+    if not task_ids:
+        new_id = 0
+    else:
+        new_id = max(task_ids) + 1
+    return new_id
+    
+
+def _update(value: str):
+    tasks = load_tasks()
+
+def _delete(value: str):
+    tasks = load_tasks()
+
+def _list(value: str):
+    tasks = load_tasks()
+
+def perform_action(action: str, value: str):
+    match action:
+        case 'add':
+            _add(value)
+        case 'update':
+            _update(value)
+        case 'delete':
+            _delete(value)
+        case 'list':
+            _list(value)
+
+def set_up_logging():
+    logging.basicConfig(level=logging.INFO, format='%(name)s: %(message)s')
+    LOGGER.debug('Started')
 
 def main():
+    set_up_logging()
     args = get_args()
-    tasks = load_task_json()
     action, value = choose_action(args)
-    print(action)
-    print(value)
-    print(tasks)
+    perform_action(action, value)
+    LOGGER.debug('Finished')
 
 if __name__ == "__main__":
     main()
